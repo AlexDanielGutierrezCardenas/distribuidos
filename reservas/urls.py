1bbad5e05a1577f0bd71d10cf8813f7cb588d4bf
AlexDanielGutierrezCardenas <gutierrezcardenasalexdaniel@gmail.com>
2023-07-02 15:51:18 -0400

    update archives

--- reservas/urls.py
+++ reservas/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     #path('',views.index, name='index'),
-    path('reserva_index',views.index,name='reserva_index'),
+    path('reserva_index',views.index_reserva,name='reserva_index'),
     path('reserva_nueva',views.nueva_reserva,name='nueva_reserva'),
 ]
--- reservas/views.py
+++ reservas/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Reserva
 
 def index_reserva(request):
-    return render(request,'reservas/index_reserva.html')
+    reservas = Reserva.objects.all()
+    return render(request,'reservas/index.html', {'reservas':reservas})
 
 def nueva_reserva(request):
     return render(request,'reservas/nueva_reserva.html')
